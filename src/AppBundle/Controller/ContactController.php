2025889cc754223d1dadfae21bb0d1b970bb8f7e
petar <petar.arandjic@asioso.com>
2020-10-15 00:17:27 +0200

    refactored content controller

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -6,9 +6,6 @@ namespace AppBundle\Controller;
 
 use Pimcore\Controller\FrontendController;
 use Pimcore\Model\DataObject\ContactMessage;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,64 +22,46 @@ class ContactController extends FrontendController
      */
     public function formAction(Config $websiteConfig, Request $request){
 
-        if ($request->server->get('HTTP_ORIGIN') != $websiteConfig->get('form_origin')){
+        if ($request->server->get('HTTP_ORIGIN') != $this->container->getParameter('corsAllowOrigin')){
 
             return new Response('', 403);
 
         }
 
-        $data = json_decode($request->getContent(), true);
+        $contactMessageFactory = $this->get('factory.contact_message');
 
-        $contactMessage = new ContactMessage();
+        $response = $contactMessageFactory->create(json_decode($request->getContent(), true));
 
-        $contactMessage->setFullName($data['full_name']);
-        $contactMessage->setEmail($data['email']);
-        $contactMessage->setMessage($data['message']);
-        $contactMessage->setParentId(2);
-        $contactMessage->setKey($data['email'] . date('c'));
-        $contactMessage->setPublished(true);
+        if ($response['saved']){
 
-        $contactMessageSaved = $contactMessage->save();
+            $pushNotification = $this->get('custom_services.wire_pusher_services');
 
-        if ($contactMessageSaved){
-
-            $response = [
-              'saved' => true
-            ];
-
-        } else {
-
-            $response = [
-                'saved' => false,
-                'errors' => [
-                    'Server error please try again in few minutes.'
-                ]
-            ];
+            $pushNotification->pushNotification(json_decode($request->getContent(),true));
 
         }
 
-        $response = new Response(
-            json_encode($response)
-            ,
+        return new Response(
+
+            json_encode($response),
+
             Response::HTTP_OK,
+
             [
-                'Access-Control-Allow-Origin' =>  'http://my-website.loc',
+                'Access-Control-Allow-Origin' => $this->container->getParameter('corsAllowOrigin'),
                 'Access-Control-Allow-Methods' => 'GET, POST',
                 'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
                 'Content-Type' => 'application/json'
             ]
         );
 
-        return $response;
-
     }
 
-    /**
-     * @Route("/contact/test")
-     */
-    public function test(){
-
-        return $this->render('Default/test.html.twig');
-
-    }
+//    /**
+//     * @Route("/contact/test")
+//     */
+//    public function test(){
+//
+//        return $this->render('Default/test.html.twig');
+//
+//    }
 }
